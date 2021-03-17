5aab0310c7cccb78554d4a8f8a51a797ae04f901
Ramon Carceles <juanramoncarceles@gmail.com>
2021-03-17 08:53:23 +0100

    rotating clockwise takes accumulated elements and wraps

--- script.js
+++ script.js
@@ -29,6 +29,11 @@ let currentTouch;
 let initialAngle = 0;
 let currentAngle;
 let angleBetweenItems = 0.6;
+/**
+ * This is the angle area of the arc that is used to ditribute items.
+ * TODO This could be dynamic depending on the case.
+ */
+const distributionAngleThreshold = Math.PI;
 
 dialItemsGroup.style.transform = "rotate(" + initialAngle + "rad)";
 
@@ -46,41 +51,80 @@ function handleStart(e) {
 
 let positionIndexOffset = 0;
 let firstItemPositionIndex = ((dialItems.length - 1) / 2) * -1; // firstVisibleItemIndex ?
+let firstVisibleItemIndex;
 let lastItemPositionIndex = (dialItems.length - 1) / 2;
 let currentLast = dialItems.length - 1;
+let currentLastVisible;
 let currentFirst = 0;
+let currentFirstVisible;
 
+/**
+ * Called when rotating clockwise.
+ * @param {*} positionOffset
+ */
 function moveLastToBeginning(positionOffset) {
-  // The first becomes the last.
-  dialItems[currentLast % 5].style.transform =
+  // Hide last visible from the right.
+  dialItems[currentLastVisible % dialItems.length].style.transform = "";
+  // TODO also display none
+  currentLastVisible--;
+  currentLastVisible =
+    currentLastVisible < 0 ? dialItems.length - 1 : currentLastVisible;
+
+  // Show new one from the left.
+  currentFirstVisible--;
+  currentFirstVisible =
+    currentFirstVisible < 0 ? dialItems.length - 1 : currentFirstVisible;
+  // firstVisibleItemIndex--; // this is not necessary then...
+  dialItems[currentFirstVisible].style.transform =
     "rotate(" +
-    (firstItemPositionIndex - positionOffset) * angleBetweenItems +
+    (firstVisibleItemIndex - positionOffset) * angleBetweenItems +
     "rad) translate(300px, 50px)";
-  currentLast--;
-  currentLast = currentLast < 0 ? dialItems.length - 1 : currentLast;
-  currentFirst--;
-  currentFirst = currentFirst < 0 ? dialItems.length - 1 : currentFirst;
-  // update firstItemIndex ?
+
+  console.log(currentFirstVisible, currentLastVisible, firstVisibleItemIndex);
 }
 
 function moveFirstToEnd(positionOffset) {
   // The last becomes the first.
-  dialItems[currentFirst % 5].style.transform =
+  dialItems[currentFirst % dialItems.length].style.transform =
     "rotate(" +
     (firstItemPositionIndex + positionOffset) * -1 * angleBetweenItems +
     "rad) translate(300px, 50px)";
   currentFirst++;
   currentLast++;
-  // update firstItemIndex ?
+
+  // update firstVisibleItemIndex ?
 }
 
 function distribute() {
   let start = firstItemPositionIndex;
+  const angleLimit = distributionAngleThreshold / 2;
+  let currentFirstVisibleCounter = 0;
+  firstVisibleItemIndex = firstItemPositionIndex;
+
   for (let item of dialItems) {
-    item.style.transform =
-      "rotate(" + start * angleBetweenItems + "rad) translate(300px, 50px)";
+    const itemAngle = start * angleBetweenItems;
+
+    if (itemAngle < -angleLimit) {
+      // Hide the item and update the counters for the next item.
+      // TODO apply just a display none
+      currentFirstVisibleCounter++;
+      firstVisibleItemIndex++;
+    } else if (itemAngle > angleLimit) {
+      // Set the positive angle limit.
+      item.style.transform =
+        "rotate(" + angleLimit + "rad) translate(300px, 50px)";
+    } else {
+      // Set the real angle.
+      item.style.transform =
+        "rotate(" + itemAngle + "rad) translate(300px, 50px)";
+    }
     start++;
   }
+
+  currentFirstVisible = currentFirstVisibleCounter;
+  currentLastVisible = dialItems.length - 1 - currentFirstVisibleCounter;
+
+  console.log(currentFirstVisible, currentLastVisible, firstVisibleItemIndex);
 }
 
 distribute();
@@ -99,12 +143,12 @@ function handleMove(e) {
       initialAngle + angle3Points(currentPoint, initialPoint, centerPoint);
 
     if (currentAngle / angleBetweenItems > positionIndexOffset + 1) {
-      console.log("clockwise shift");
       positionIndexOffset = Math.floor(currentAngle / angleBetweenItems);
+      console.log("clockwise shift", positionIndexOffset);
       moveLastToBeginning(positionIndexOffset);
     } else if (currentAngle / angleBetweenItems < positionIndexOffset - 1) {
-      console.log("counterclockwise shift");
       positionIndexOffset = Math.ceil(currentAngle / angleBetweenItems);
+      console.log("counterclockwise shift");
       moveFirstToEnd(positionIndexOffset);
     }
 
